4935ee504e0224cbc97365a753a5654a9014f8a2
ro <mail2benr@gmail.com>
2019-12-13 16:43:33 +0100

    Update docstrings

--- satpy/scene.py
+++ satpy/scene.py
@@ -1358,21 +1358,27 @@ class Scene(MetadataObject):
         return writer.save_datasets(datasets, compute=compute, **save_kwargs)
 
     def compute(self, **kwargs):
-        """Calls :meth:`xarray.DataArray.compute` on all Scene datasets."""
+        """Calls `compute()` on all Scene datasets.
+        See :meth:`xarray.DataArray.compute` for more details.
+        """
         new_scn = self.copy()
         for k in new_scn.datasets.keys():
             new_scn[k] = new_scn[k].compute(**kwargs)
         return new_scn
 
     def persist(self, **kwargs):
-        """Calls :meth:`xarray.DataArray.persist` on all Scene datasets."""
+        """Calls `persist()` on all Scene datasets.
+        See :meth:`xarray.DataArray.persist` for more details.
+        """
         new_scn = self.copy()
         for k in new_scn.datasets.keys():
             new_scn[k] = new_scn[k].persist(**kwargs)
         return new_scn
 
     def chunk(self, **kwargs):
-        """Calls :meth:`xarray.DataArray.chunk` on all Scene datasets."""
+        """Calls `chunk()` on all Scene datasets.
+        See :meth:`xarray.DataArray.chunk` for more details.
+        """
         new_scn = self.copy()
         for k in new_scn.datasets.keys():
             new_scn[k] = new_scn[k].chunk(**kwargs)
